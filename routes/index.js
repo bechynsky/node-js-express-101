51dfd8d7ff6cfd62cd1211848eed88cf63833750
Štěpán Bechynský <stepan@bechynsky.cz>
2023-03-22 13:36:16 +0100

    Read settings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var dbpwd = process.env.DB_PASSWORD;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', databasepassword: dbpwd });
 });
 
 module.exports = router;
